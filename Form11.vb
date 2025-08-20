b619d6d750f103862d97d68d62ced05cfcb64e9f
z181k <z181k@hotmail.com>
2025-08-21 00:43:52 +0800

    清除数据时会检查winappsdk注册表值

--- Form11.vb
+++ Form11.vb
@@ -8,9 +8,19 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        On Error GoTo nexttap
         If MsgBox("是否要清除数据？此操作会清除应用所有设置并关闭应用。", 308, "清除数据并关闭程序") = vbYes Then
+            Dim CreateWinAppSdkUi As String = "False"
+            Dim WinAppSdkUi As String = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\DBT\WFL Tool", "WinAppSdkUi", Nothing)
+            If WinAppSdkUi = "1" Then
+                CreateWinAppSdkUi = "True"
+            End If
+nexttap:
             Shell("reg.exe delete ""HKEY_CURRENT_USER\Software\DBT\WFL Tool"" /va /f", AppWinStyle.Hide, True, -1)
             Shell("cmd.exe /c rd ""%localappdata%\WFL Tool\EWV2viewer\EWV2viewer.exe.webview2"" /s /q", AppWinStyle.Hide, True, -1)
+            If CreateWinAppSdkUi = "True" Then
+                Shell("reg.exe add ""HKEY_CURRENT_USER\Software\DBT\WFL Tool"" /v WinAppSdkUi /T REG_SZ /d 1 /f", AppWinStyle.Hide, True, -1)
+            End If
             End
         End If
     End Sub
