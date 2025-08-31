a414a958a398dec6e09eb2abf3c33f902d9e99c1
z181k <z181k@hotmail.com>
2025-08-31 21:55:44 +0800

    协议再更新

--- Form12.Designer.vb
+++ Form12.Designer.vb
@@ -34,21 +34,23 @@ Partial Class Form12
         '
         Me.TextBox1.AcceptsReturn = True
         Me.TextBox1.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Location = New System.Drawing.Point(22, 21)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.ReadOnly = True
         Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.TextBox1.Size = New System.Drawing.Size(376, 391)
+        Me.TextBox1.Size = New System.Drawing.Size(686, 681)
         Me.TextBox1.TabIndex = 4
         Me.TextBox1.Text = "由代码控制内容"
         '
         'Button1
         '
         Me.Button1.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.Button1.Location = New System.Drawing.Point(206, 409)
+        Me.Button1.Location = New System.Drawing.Point(378, 716)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(88, 32)
+        Me.Button1.Size = New System.Drawing.Size(161, 56)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "不接受"
         Me.Button1.UseVisualStyleBackColor = True
@@ -56,9 +58,10 @@ Partial Class Form12
         'Button2
         '
         Me.Button2.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.Button2.Location = New System.Drawing.Point(300, 409)
+        Me.Button2.Location = New System.Drawing.Point(550, 716)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(88, 32)
+        Me.Button2.Size = New System.Drawing.Size(161, 56)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "接受"
         Me.Button2.UseVisualStyleBackColor = True
@@ -66,9 +69,10 @@ Partial Class Form12
         'Button3
         '
         Me.Button3.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.Button3.Location = New System.Drawing.Point(12, 409)
+        Me.Button3.Location = New System.Drawing.Point(22, 716)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(181, 32)
+        Me.Button3.Size = New System.Drawing.Size(332, 56)
         Me.Button3.TabIndex = 3
         Me.Button3.Text = "前往 WFL Tool 选项取消授权"
         Me.Button3.UseVisualStyleBackColor = True
@@ -78,29 +82,31 @@ Partial Class Form12
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.Label1.Location = New System.Drawing.Point(22, 417)
+        Me.Label1.Location = New System.Drawing.Point(40, 730)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(46, 17)
+        Me.Label1.Size = New System.Drawing.Size(78, 28)
         Me.Label1.TabIndex = 5
         Me.Label1.Text = "Label1"
         Me.Label1.Visible = False
         '
         'Form12
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 21.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.ClientSize = New System.Drawing.Size(400, 454)
+        Me.ClientSize = New System.Drawing.Size(733, 794)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "Form12"
-        Me.Text = "软件许可协议 - 内容更新日期：2025/08/21 - WFL Tool"
+        Me.Text = "软件许可协议 - 内容更新日期：2025/08/30 - WFL Tool"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
