3e73c43b339fb733f74019777f4c372316e4fa89
z181k <z181k@hotmail.com>
2026-02-02 15:46:33 +0800

    部分场景不再渲染更改安装进度页

--- IoU/WFL Tool IoU/SetupUserModify.vb
+++ IoU/WFL Tool IoU/SetupUserModify.vb
@@ -5,12 +5,12 @@
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Me.Visible = False
-        SetupUserInstall.Show()
-        SetupUserInstall.Text = "更改 WFL Tool (x64) 的安装"
-        SetupUserInstall.Label4.Text = "更改安装"
-        SetupUserInstall.Label1.Text = "正在更改"
-        SetupUserInstall.PB1.Value = 88
         If CheckBox1.Checked = True And CheckBox1.Enabled = True Then
+            SetupUserInstall.Show()
+            SetupUserInstall.Text = "更改 WFL Tool (x64) 的安装"
+            SetupUserInstall.Label4.Text = "更改安装"
+            SetupUserInstall.Label1.Text = "正在更改"
+            SetupUserInstall.PB1.Value = 95
             MsgBox("卸载 Edge WebView2 渲染支持组件仅对当前安装的当前版本有效，不会清除该组件的数据，更新或重新安装应用时会自动重新安装该组件。"， vbInformation, "更改 WFL Tool (x64) 的安装")
             Shell("cmd.EXE /c del ""%localappdata%\WFL Tool\EWV2viewer\*.*"" /q", AppWinStyle.Hide, False, -1)
             Shell("cmd.EXE /c rd ""%localappdata%\WFL Tool\EWV2viewer\deverrcodehlp_files"" /s /q", AppWinStyle.Hide, False, -1)
@@ -27,7 +27,6 @@
                 Shell("reg.exe add ""HKEY_CURRENT_USER\Software\DBT\WFL Tool"" /v WinAppSdkUi /T REG_SZ /d 1 /f", AppWinStyle.Hide, True, -1)
             End If
         End If
-        SetupUserInstall.PB1.Value = 100
         Application.Exit()
     End Sub
 
