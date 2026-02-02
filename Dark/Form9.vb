1abad5cb2dc5b546ac839c92363470187b96290f
z181k <z181k@hotmail.com>
2026-02-02 15:37:03 +0800

    解决使用 Webview2 页面在部分低分辨率屏幕下的显示问题

--- Dark/Form9.vb
+++ Dark/Form9.vb
@@ -4,7 +4,7 @@ Public Class Form9
 
 
     Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
-        Dim upginfo As String = "RTM 版本(9504.1):" & vbCrLf & vbCrLf & "-新的：" & vbCrLf & "1." & vbCrLf & "2." & vbCrLf & vbCrLf & "-修复：" & vbCrLf & "1." & vbCrLf & "2.解决上个版本修复内容对 WinServer 2022 无效问题" & vbCrLf & "3.解决和卸载程序有关的多个问题" & vbCrLf & ""     '此处换行为了解决 Messagebox.exe 吞行问题
+        Dim upginfo As String = "RTM 版本(9504.1):" & vbCrLf & vbCrLf & "-新的：" & vbCrLf & "1." & vbCrLf & "2." & vbCrLf & vbCrLf & "-修复：" & vbCrLf & "1." & vbCrLf & "2.解决上个版本修复内容对 WinServer 2022 无效问题" & vbCrLf & "3.解决和卸载程序有关的多个问题" & vbCrLf & "4.解决使用 Webview2 页面在部分低分辨率屏幕下的显示问题" & vbCrLf & ""     '此处换行为了解决 Messagebox.exe 吞行问题
         '所有标两个引号的是在Windows 7及以下系统存在严重问题的功能可控启用模块代码
         ''If FeatureControlLabel1.Text = "enable" Then    '判断相关功能是否可以使用
         ''   此处放启用后日志
--- EWV2viewer/EWV2webpage.vb
+++ EWV2viewer/EWV2webpage.vb
@@ -22,16 +22,22 @@ Public Class EWV2webpage
         'url地址
         Dim EWV2webpageURL As String = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("software\DBT\WFL Tool", True).GetValue("EWV2webpageURL", "https://player.bilibili.com/player.html?isOutside=true&aid=291456358&bvid=BV1sf4y1b74k&cid=361932939&p=1")
         WebView21.Source = New Uri(EWV2webpageURL)
-        '清理信息
-        Shell("reg.exe add ""HKEY_CURRENT_USER\Software\DBT\WFL Tool"" /v EWV2webpageShow /T REG_SZ /d 6 /f", AppWinStyle.Hide, True, -1)
-        Shell("reg.exe add ""HKEY_CURRENT_USER\Software\DBT\WFL Tool"" /v EWV2webpageShowIcon /T REG_SZ /d True /f", AppWinStyle.Hide, True, -1)
-        Shell("reg.exe add ""HKEY_CURRENT_USER\Software\DBT\WFL Tool"" /v EWV2webpageTitle /T REG_SZ /d 注意！这是第三方页面 /f", AppWinStyle.Hide, True, -1)
-        Shell("reg.exe add ""HKEY_CURRENT_USER\Software\DBT\WFL Tool"" /v EWV2webpageURL /T REG_SZ /d ""https://liulanmi.com/core"" /f", AppWinStyle.Hide, True, -1)
+        Dim EWV2webpageNotClean As String = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("software\DBT\WFL Tool", True).GetValue("EWV2webpageNotClean", "False")
+        If EWV2webpageNotClean <> "True" Then
+            '清理信息
+            Shell("reg.exe add ""HKEY_CURRENT_USER\Software\DBT\WFL Tool"" /v EWV2webpageShow /T REG_SZ /d 6 /f", AppWinStyle.Hide, True, -1)
+            Shell("reg.exe add ""HKEY_CURRENT_USER\Software\DBT\WFL Tool"" /v EWV2webpageShowIcon /T REG_SZ /d True /f", AppWinStyle.Hide, True, -1)
+            Shell("reg.exe add ""HKEY_CURRENT_USER\Software\DBT\WFL Tool"" /v EWV2webpageTitle /T REG_SZ /d 注意！这是第三方页面 /f", AppWinStyle.Hide, True, -1)
+            Shell("reg.exe add ""HKEY_CURRENT_USER\Software\DBT\WFL Tool"" /v EWV2webpageURL /T REG_SZ /d ""https://liulanmi.com/core"" /f", AppWinStyle.Hide, True, -1)
+        End If
     End Sub
     Private Sub HelpEWV2_sizechange(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.SizeChanged
-        If SizeChangeFlg Then
-            asc.ControlAutoSize(Me)
-        End If
+        WebView21.Width = Me.Width - 24
+        WebView21.Height = Me.Height - 63
+        '旧的方案
+        'If SizeChangeFlg Then
+        '    asc.ControlAutoSize(Me)
+        'End If
         'Button5.Text = Me.Width & ":" & Me.Height '利用控件名称显示的窗体大小，可删
     End Sub
 
