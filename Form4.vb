a94da3fbb5af943f253be4ab23dc9cebc2100865
z181k <z181k@hotmail.com>
2026-02-15 11:37:48 +0800

    清理废弃代码

--- Form4.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Form4
-
-    Private Sub 返回主界面ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 返回主界面ToolStripMenuItem.Click
-        Form1.Show()
-    End Sub
-End Class
